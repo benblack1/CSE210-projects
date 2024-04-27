7db3ebd382d0dd1a9b1f1cb2756940118042f051
Ian Black <eeian09@gmail.com>
2024-04-27 14:20:59 -0600

    Finished assignment Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,100);
+        bool wrongAnswer = true;
+        
+
+        while (wrongAnswer){
+            Console.Write("Please guess a number: ");
+            string numberString = Console.ReadLine();
+            int number = int.Parse(numberString);
+            if (number == randomNumber){
+                Console.WriteLine("Yay! You guessed it!");
+                wrongAnswer = false;
+            }
+            else if (number > randomNumber){
+                Console.WriteLine("Guess lower");
+            }
+            else if (number < randomNumber){
+                Console.WriteLine("Guess higher");
+            }
+            
+        }
     }
 }
