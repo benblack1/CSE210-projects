db748330d4bd836732f37468a2da8e1b2325d562
Ian Black <eeian09@gmail.com>
2024-04-27 16:35:14 -0600

    Finished assignment Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        // Declare all the variables
+        List<int> numbers= new List<int>();
+        bool notFinished = true;
+        int total = 0;
+        int totalCount = 0;
+        int largestToDate = 0;
+        Console.Write("Enter a number. Hit 0 when you are finished.");
+
+        //I wanted to do all the counting and calculations in the while loop and
+        //not pull in a foreach loop just for the heck of it, to 
+        //prove to myself that I could.  
+        while (notFinished){
+            string userInput = Console.ReadLine();
+            int nextItem = int.Parse(userInput);
+            if (nextItem != 0){
+                numbers.Add(nextItem);
+                total += nextItem;
+                totalCount ++;
+                if (nextItem > largestToDate){
+                    largestToDate = nextItem;
+                }
+            }
+            else{
+                notFinished = false;
+            }
+        }
+        
+        float average = total/totalCount;
+        Console.WriteLine($"The sum is {total}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number was {largestToDate}");
     }
 }
