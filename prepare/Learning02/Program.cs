e644e915a8fe2f594ff651801f15ad0b67a4ad0d
Ian Black <eeian09@gmail.com>
2024-04-30 22:49:51 -0600

    Finished the Week Two Prepare Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Senior Cheif Software Engineer over Operations";
+        job1._company = "Google";
+        job1._endYear = 2021;
+        job1._startYear = 2010;
+        //job1.Display();
+        
+        Job job2 = new Job();
+        job2._endYear = 2024;
+        job2._startYear = 2021;
+        job2._company = "Microsoft";
+        job2._jobTitle = "Unpaid Intern";
+
+        //job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Joe Schmoe ";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Resume{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    public void Display(){
+        Console.Write($"{_name}");
+        foreach (Job job in _jobs){
+            job.Display();
+        }
+    }
+}
