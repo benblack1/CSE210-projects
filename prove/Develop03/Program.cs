9b97403a514b200b25192b638af529bb3fb0cb56
Ben Black <benblack068@gmail.com>
2024-05-11 21:20:10 -0600

    Bug fix

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,10 +6,12 @@ public class Program
     {
         Scripture scripture = new Scripture("Proverbs 3:5-6","Trust in the LORD with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths");
         
+        Console.Clear();
         scripture.DisplayScripture();
 
         string input = string.Empty;
-        while (input != "Quit")
+        input = Console.ReadLine();
+        while (input != "quit")
         {
             scripture.HideRandomVerses();
             input = Console.ReadLine();
